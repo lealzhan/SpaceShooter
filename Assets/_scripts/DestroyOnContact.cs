8df0eec844d22e4ca408510a4cb20d4d32770fb2
lealzhan <lealzhan@126.com>
2021-06-07 23:49:04 +0800

    添加陨石：随机旋转，和子弹及飞船碰撞消失

---
+++ Assets/_scripts/DestroyOnContact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //print(other.name);
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
